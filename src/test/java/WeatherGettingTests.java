484be84b3ddf70148ffb856636d739dec9dc1c76
u4s5 <u4s5@yandex.ru>
2017-05-23 02:07:18 +0300

    Initial commit

---
+++ src/test/java/WeatherGettingTests.java
@@ -0,0 +1,91 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class WeatherGettingTests {
+
+    private final long DELAY = 500;
+
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+
+    @BeforeClass
+    public static void init() {
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://www.gismeteo.ru";
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testGettingTodayWeather() throws Exception {
+        driver.get(baseUrl + "/");
+        driver.findElement(By.xpath("//a[@class='nolink subnav_item'][1]")).click();
+        Thread.sleep(DELAY);
+
+        assertEquals(driver.getCurrentUrl(), "");
+    }
+
+    @Test
+    public void testGettingTomorrowWeather() throws Exception {
+        driver.get(baseUrl + "/");
+        driver.findElement(By.xpath("//a[@class='nolink subnav_item'][2]")).click();
+        Thread.sleep(DELAY);
+
+        assertEquals(driver.getCurrentUrl(), "");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
