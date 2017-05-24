b84528d385eba406ac840e1db242a386d3ba1b9a
u4s5 <u4s5@yandex.ru>
2017-05-24 16:31:57 +0300

    Continue fixing DefaultLocationSetter

--- src/test/java/utils/DefaultLocationSetter.java
+++ src/test/java/utils/DefaultLocationSetter.java
@@ -1,11 +1,21 @@
 package utils;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 public class DefaultLocationSetter {
 
     public static void setDefaultLocation(WebDriver driver, long DELAY) throws Exception {
+        driver.findElement(By.xpath("//a[@class='link blue no_border'][1]")).click();
+        Thread.sleep(DELAY);
+
+        driver.findElement(By.xpath("//div[@class='currentgeo_description'][1]")).click();
+        Thread.sleep(DELAY);
+
+        driver.findElement(By.xpath("//input[@class='search_input'][1]")).sendKeys("Санкт-Петербург");
+        Thread.sleep(DELAY);
+
         driver.navigate().to("https://www.gismeteo.ru/search/Санкт-Петербург/");
         Thread.sleep(DELAY);
 
