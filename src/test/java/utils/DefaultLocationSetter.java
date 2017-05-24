5dc4f2f48bea25d9012f361ae956f29921224aa6
u4s5 <u4s5@yandex.ru>
2017-05-24 17:36:29 +0300

    Final fix of DefaultLocationSetter (I believe)

--- src/test/java/utils/DefaultLocationSetter.java
+++ src/test/java/utils/DefaultLocationSetter.java
@@ -10,16 +10,14 @@ public class DefaultLocationSetter {
         driver.findElement(By.xpath("//a[@class='link blue no_border'][1]")).click();
         Thread.sleep(DELAY);
 
-        driver.findElement(By.xpath("//div[@class='currentgeo_description'][1]")).click();
+        driver.findElements(By.name("currentgeo")).get(2).click();
         Thread.sleep(DELAY);
-
         driver.findElement(By.xpath("//input[@class='search_input'][1]")).sendKeys("Санкт-Петербург");
         Thread.sleep(DELAY);
 
-        driver.navigate().to("https://www.gismeteo.ru/search/Санкт-Петербург/");
+        driver.findElement(By.xpath("//input[@class='search_input'][1]")).sendKeys(Keys.ARROW_DOWN);
         Thread.sleep(DELAY);
-
-        driver.findElement(By.xpath("//section[@class='catalog_block'][2]//div[@class='catalog_item'][1]/a[@class='catalog_link link blue fontM'][1]")).click();
+        driver.findElement(By.xpath("//input[@class='search_input'][1]")).sendKeys(Keys.RETURN);
         Thread.sleep(DELAY);
     }
 
